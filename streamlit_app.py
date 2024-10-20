4bf6e88c343fcea7d68611f660c03cbda4da949d
Subin Han <hsubin97@gmail.com>
2024-10-20 19:38:31 +0900

    solve overload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,22 @@ import streamlit as st
 import json
 import checkitoutAPI
 import pandas as pd
+import os, time
 
-reviews=checkitoutAPI.get_reviews()
+data_path='reviews.json'
+get_data=True
+if os.path.exists(data_path):
+    file_time=os.path.getctime(data_path)
+    current_time=time.time()
+    one_hour=current_time-3600
+    if file_time< one_hour:
+        get_data=False
+if get_data:
+    reviews=checkitoutAPI.get_reviews()
+    checkitoutAPI.reviews_tojson(reviews)
+else:
+    with open('reviews.json', 'r', encoding='utf-8') as f:
+        reviews=json.load(f)
 
 st.title("🎈 책키라웃 리뷰 Summary Page!")
 review_summary=[rv.summary for rv in reviews]
